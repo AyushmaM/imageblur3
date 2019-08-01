42aabc8532a1998f09438ec86148d91909b891e4
AyushmaM <ayushmamalla16@gmail.com>
2019-08-01 01:27:06 +0000

    Initial commit

---
+++ imageblur3.rb
@@ -0,0 +1,52 @@
+class Image
+
+  attr_accessor :image
+
+  def initialize(pixel)
+    @image = pixel
+  end
+
+  def output_image
+    @image.each do |num|
+      puts num.join
+    end
+  end
+
+  def find_1!
+    ones = []
+    @image.each_with_index do |arr, row_index|
+
+      arr.each_with_index do |element, column_index|
+        ones << [row_index, column_index] if element == 1
+        
+      end
+    end
+    #puts ones.inspect
+
+    ones.each do |blur|
+     x = blur[0]
+    y = blur[1]
+
+     @image[x][y + 1] = 1 if y + 1 <= @image[x].count - 1
+     @image[x][y - 1] = 1 if y - 1 >= 0
+     @image[x + 1][y] = 1 if x + 1 <= @image.count - 1
+     @image[x - 1][y] = 1 if x - 1 >= 0
+
+    end
+
+  end
+
+
+end
+
+image = Image.new([
+  [0, 0, 0, 0], 
+  [0, 1, 0, 0], 
+  [0, 0, 0, 1],
+  [0, 0, 0, 0]
+])
+
+
+image.find_1!
+image.output_image
+
